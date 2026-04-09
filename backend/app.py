78a341804e2bd7096e4669e0116fc91ceb47cadf
idleshivam <shivamjoshi096@gmail.com>
2026-04-10 02:02:32 +0530

    fix PORT

--- backend/app.py
+++ backend/app.py
@@ -37,6 +37,7 @@ def routes():
 
     return jsonify(routes)
 
-#porting the webpage 
+#porting the webpage
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
